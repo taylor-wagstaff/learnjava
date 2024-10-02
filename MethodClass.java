fcce1e954a37f29d350f4736a00eac52984b28e9
Taylor Wagstaff <taylorwagstaff@gmail.com>
2024-10-02 22:38:20 +0100

    java

---
+++ MethodClass.java
@@ -0,0 +1,38 @@
+class MethodClass {
+  int passengers; // number of passengers
+  int fuelcap; // fuel capacity in gallons
+  int mpg; // fuel consumption in miles per gallon
+  // Display the range.
+
+  void range() {
+
+    // The range( ) method is contained within the Vehicle class.
+    System.out.println("Range is " + fuelcap * mpg);
+    // Notice that fuelcap and mpg are used directly, without the dot operator.
+  }
+}
+
+class AddMeth {
+  public static void main(String[] args) {
+    MethodClass minivan = new MethodClass();
+    MethodClass sportscar = new MethodClass();
+    int range1, range2;
+
+    // assign values to fields in minivan
+    minivan.passengers = 7;
+    minivan.fuelcap = 16;
+    minivan.mpg = 21;
+
+    // assign values to fields in sportscar
+    sportscar.passengers = 2;
+    sportscar.fuelcap = 14;
+    sportscar.mpg = 12;
+
+    System.out.print("Minivan can carry " + minivan.passengers +
+        ". ");
+    minivan.range(); // display range of minivan
+    System.out.print("Sportscar can carry " + sportscar.passengers +
+        ". ");
+    sportscar.range(); // display range of sportscar.
+  }
+}
